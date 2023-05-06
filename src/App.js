0a7b3e1adf19b5c6f0f0e1a53b5dc513b5f8b357
jaycee808 <jamie.clark@live.co.uk>
2023-05-06 16:21:34 +0100

    added api searches

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+// API searches
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2f1ce84a1413ea3ccdba3439dfef68e2';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=2f1ce84a1413ea3ccdba3439dfef68e2&query=';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Call getMovies function
+  useEffect(() => {
+    getMovies(API_URL);
+  }, []);
+
+  // getMovies function
+  const getMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovies(data.results);
+  };
+
+
   return (
-    <div className="App">
-      <h1 id="logo">CINEFIX.</h1>
-    </div>
+    <>
+      <header>
+      <h1 id="logo">cinefix.</h1>
+      </header>
+    </>
   );
-}
+};
 
 export default App;
